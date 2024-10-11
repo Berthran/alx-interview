36877391341dc9a5d2bef026ba1ff02ceedc963f
Daniel Berthran <berthlearn@gmail.com>
2024-10-11 15:54:05 +0100

    restructure code

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -8,30 +8,24 @@
 def checkUnaccessedBoxes(unaccessedBoxes, unlockedBoxes, lockedBoxes, boxes):
     if unaccessedBoxes:
         unaccessed = []
-        # print("Calling")
+
         for index in unaccessedBoxes:
-            # print('index', index)
             if index in unlockedBoxes:
-                # print('index {} accessed'.format(index))
-                for key in boxes[index]:
-                    if key != 0 and key <= lockedBoxes[-1]:
-                        if (key not in unlockedBoxes):
-                            unlockedBoxes.append(key)
-                            unlockedBoxes.sort()
+                box = boxes[index]
+                for key in box:
+                    if key not in unlockedBoxes and key <= lockedBoxes[-1] \
+                                                and key != 0:
+                        unlockedBoxes.append(key)
+                unlockedBoxes.sort()
             else:
                 unaccessed.append(index)
-                # print('printUnaccessed', unaccessed)
+
         if unaccessed and (unaccessed != unaccessedBoxes):
-            # print('Going back for', unaccessed)
             checkUnaccessedBoxes(unaccessed, unlockedBoxes, lockedBoxes, boxes)
         else:
-            # print('unlocked boxes after recursion', unlockedBoxes)
-            # print('Done with recursion')
-            # print(f'returning {unlockedBoxes} of type {type(unlockedBoxes)}')
             return unlockedBoxes
+
     else:
-        # print('unlocked boxes no recursion', unlockedBoxes)
-        # print('No recursion')
         return unlockedBoxes
 
 
@@ -41,41 +35,29 @@ def canUnlockAll(boxes):
     '''
     lockedBoxes = [i for i in range(len(boxes))][1:]
     unlockedBoxes = []
-    # print('locked boxes', lockedBoxes)
-    # print('unlocked boxes', unlockedBoxes)
-    # print(len(boxes))
-    # exit()
-    # print('locked boxes', lockedBoxes)
-    # print('unlocked boxes', unlockedBoxes)
     unaccessedBoxes = []
 
+    # Empty box or box with only on
     if lockedBoxes == []:
         return True
 
     for index, box in enumerate(boxes):
-        # print("Index {}".format(index))
-        for key in box:
-            if index == 0 or index in unlockedBoxes:
-                if key != 0 and key <= lockedBoxes[-1]:
-                    if key not in unlockedBoxes:
-                        unlockedBoxes.append(key)
-                        unlockedBoxes.sort()
-            else:
-                unaccessedBoxes.append(index)
-                break
-        if unlockedBoxes == lockedBoxes:
-            # print('unlocked boxes', unlockedBoxes)
-            # print('no need to go further')
-            return True
+        if index == 0 or index in unlockedBoxes:
 
-    # print('locked', lockedBoxes)
-    # print('unlocked', unlockedBoxes)
-    # print('reserve', unaccessedBoxes)
+            for key in box:
+                if key not in unlockedBoxes and key <= lockedBoxes[-1] \
+                                            and key != 0:
+                    unlockedBoxes.append(key)
+            unlockedBoxes.sort()
+
+            if unlockedBoxes == lockedBoxes:
+                # print('no need to go further')
+                return True
+        else:
+            unaccessedBoxes.append(index)
 
     checkUnaccessedBoxes(unaccessedBoxes, unlockedBoxes, lockedBoxes, boxes)
 
-    # print('unlocked boxes', unlockedBoxes)
-    # print('unlocked boxes', unlockedBoxes)
-    if unlockedBoxes == lockedBoxes or len(lockedBoxes) == 0:
+    if unlockedBoxes == lockedBoxes:
         return (True)
     return (False)
