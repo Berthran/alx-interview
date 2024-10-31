d85999a2ec52d5eea05491166df2049c504d0dd3
Daniel Berthran <berthlearn@gmail.com>
2024-10-31 19:07:08 +0100

    first commit

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+'''
+Validate if data is utf-8
+'''
+
+
+def isascii(codepoint):
+    '''
+    Checks if a byte is an ascii character
+    '''
+    if codepoint >> 7 == 0:
+        return True
+    return False
+
+
+def istwobytes(codepoint):
+    pass
+
+
+def isthreebytes(codepoint):
+    pass
+
+
+def isfoutbytes(codepoint):
+    pass
+
+
+def validUTF8(data):
+    '''
+    Checks if data is a valid UTF-8 encoding
+    '''
+    if data is None:
+        return False
+    for codepoint in data:
+        if isascii(codepoint):
+            continue
+        elif istwobytes(codepoint):
+            continue
+        elif isthreebytes(codepoint):
+            continue
+        elif isfoutbytes(codepoint):
+            continue
+        else:
+            return False
+    return True
