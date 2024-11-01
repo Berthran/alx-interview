ada53c6a526d414a6df67a7d37e6f19747dd0c7b
Daniel Berthran <berthlearn@gmail.com>
2024-11-01 12:30:06 +0100

    clean up unwanted functions

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -4,45 +4,15 @@ Validate if data is utf-8
 '''
 
 
-def isascii(codepoint):
-    '''
-    Checks if a byte is an ascii character
-    '''
-    if codepoint >> 7 == 0:
-        return True
-    return False
-
-
-def utf8range(codepoint):
-    '''
-    checks that a codepoint is in utf8range
-    '''
-    # Mask the first 8 bits of codepoint
-    mask = (1 << 8) - 1
-    if codepoint & mask <= 255:
-        return True
-    return False
-
-
-def istwobytes(codepoint):
-    pass
-
-
-def isthreebytes(codepoint):
-    pass
-
-
-def isfoutbytes(codepoint):
-    pass
-
-
 def validUTF8(data):
     '''
     Checks if data is a valid UTF-8 encoding
     '''
     i = 0
+
     if data is None:
         return False
+
     while i < len(data):
         # Get the codepoint
         codepoint = data[i]
